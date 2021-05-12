42903c86d084c3e11b9074c76b8913aab862bbd9
caralocke <cara.m.locke@gmail.com>
2021-05-12 17:36:35 -0400

    Imported axios, created my base url and api keys in constants and imported them as well.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import axios from 'axios'
+import {BASE_URL, API_KEY} from './constants'
 import "./App.css";
 
 function App() {
+  // const [nasaData, setNasaData] = useState()
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
+      Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
     </div>
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/#apod
+export const API_KEY = 'DEMO_KEY'
