1a4e79139955c80172a683066dda757c2775b226
caralocke <cara.m.locke@gmail.com>
2021-06-08 19:54:08 -0400

    Added some components I think I'll want for this project as well as rendered them to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+import {BASE_URL, API_KEY} from './constants'
+import Picture from './components/Picture'
+import Details from './components/Details'
+import Date from './components/Date'
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState([])
+  useEffect(() => {
+    axios
+    .get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+      console.log('res.data:', res.data)
+      setNasaData(res.data)
+    })
+    .catch(err => {
+      console.log(`Here's where you messed up: \n`, err)
+    })
+  },[])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Date key={nasaData.date} nasaData={nasaData}/>
+      <Picture key={nasaData.hdurl} nasaData={nasaData}/>
+      <Details key={nasaData.explanation} nasaData={nasaData}/>
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = `https://api.nasa.gov/planetary/apod`
+export const API_KEY = `LA8eHbRE9evFjVSQtmjciTsQgUktNJ0nb9CFGvHT`
