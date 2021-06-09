494893e6b5ab8043c9f6f5f8262baaf08b2b66f1
caralocke <cara.m.locke@gmail.com>
2021-06-08 20:02:51 -0400

    Was able to get the image to properly display on Picture.js

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Picture() {
+export default function Picture(props) {
+    const {nasaData} = props
     return (
         <div>
-            
+            <img src={nasaData.hdurl} alt='astronomy for today'/>
         </div>
     )
 }
