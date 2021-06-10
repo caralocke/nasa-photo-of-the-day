5d3b509bae53eda98d350429fce7ff88f9b6edfe
caralocke <cara.m.locke@gmail.com>
2021-06-10 10:52:14 -0400

    Added some styling to the image on Picture.js

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledPicture = styled.img`
+    width:800px;
+    
+    &:hover {
+        border-radius: 50%;
+        transform: scale(1.1)
+    }
+`
 
 export default function Picture(props) {
     const {nasaData} = props
     return (
         <div>
-            <img src={nasaData.hdurl} alt='astronomy for today'/>
+            <StyledPicture src={nasaData.hdurl} alt='astronomy for today'/>
         </div>
     )
 }
